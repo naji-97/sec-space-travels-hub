3a031e95833fdc27fcebcabe79365d3bb1adf1fc
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-05-15 16:02:29 +0000

    Add website header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+import Missions from './components/Mission';
+import RocketList from './components/Rockets';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/style.scss';
 
 function App() {
   return (
-    <h1> Trave</h1>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<RocketList />} />
+        <Route path="/Missions" element={<Missions />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
-
 export default App;
